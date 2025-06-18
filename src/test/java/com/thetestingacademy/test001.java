8ac7898407f42e5d0ff0474966b41b952bc3b550
ankita <ankitabhardwaj7987@gmail.com>
2025-06-18 21:23:40 +0530

    2 files are added

---
+++ src/test/java/com/thetestingacademy/test001.java
@@ -0,0 +1,9 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class test001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/thetestingacademy/test002.java
@@ -0,0 +1,25 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class test002 {
+    public static void main(String[] args) {
+        System.out.println("rest assured test case");
+        System.out.println("get request demo");
+
+        //gherkins syntax
+        // given() - URL ,headers, body or payload
+        // when()- https methods - get, post, put, patch, delete
+        // then()- verify the response - er == ar
+        RestAssured
+                .given()
+                      .baseUri("https://restful-booker.herokuapp.com")
+                      .basePath("/booking/1").log().all()
+                .when()
+                      .get()
+                .then()
+                      .statusCode(404);
+
+
+    }
+}
